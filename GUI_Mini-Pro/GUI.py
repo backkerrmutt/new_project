15bfac9f33204b1102b3bbfb4a29e7be574e0871
backker <backker@mail.com>
2024-09-17 12:50:18 +0700

    New Version

--- GUI_Mini-Pro/GUI.py
+++ GUI_Mini-Pro/GUI.py
@@ -1,1 +1,21 @@
-print("hi")
+import tkinter as tk
+
+# สร้างหน้าต่างหลัก
+root = tk.Tk()
+
+#ขนากหน้าต่างหลัก
+root.geometry("500x500")
+
+root.title("Tkinter Demo")
+
+# สร้าง label
+label = tk.Label(root, text="Hello from Tkinter")
+label.pack()
+
+# สร้างปุ่ม
+button = tk.Button(root, text="OK", command=root.quit )
+button.place(x=230, y=460)
+
+
+# เริ่มต้น main loop
+root.mainloop()
